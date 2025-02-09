40349c7123e2950682b98ba905dc5d5095729c9f
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-02-10 01:19:06 +0300

    Add dependancies and styling.

---
+++ src/components/Homepage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import NavigationBar from "./Navbar";
+import Footer from "./Footer";
+
+const HomePage = () => {
+    return (
+        <div>
+           < NavigationBar />
+
+           <section className="banner">
+           
+           </section>
+
+           <section className="testimonials">
+            
+           </section>
+           <hr/>
+           
+           <Footer />
+
+        </div>
+    );
+};
+export default HomePage;
